d992367004dc5390e3eab20b1a44e1e48e2ef0e3
saurav gaikwad <sauravgaikwad8@gmail.com>
2023-02-02 08:48:26 +0530

    hello

---
+++ MainExample.java
@@ -0,0 +1,13 @@
+package Entity;
+
+public class MainExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		hello.getMessage();
+		
+		
+	}
+
+}
---
+++ hello.java
@@ -0,0 +1,10 @@
+package Entity;
+
+public class hello {
+  
+	public static void getMessage() {
+		System.out.println("hello wolrd from infoway ");
+	}
+	
+	
+}
